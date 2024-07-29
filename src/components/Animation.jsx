5125ae3104738034e29793fced67bb6ca6e75b24
devabdulsalam <ammuftau74@gmail.com>
2024-07-29 17:11:19 +0100

    improve ui

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,28 +1,30 @@
-import PropTypes from "prop-types";
-// const COLORS = [
-//   "#bbf7d0",
-//   "#99f6e4",
-//   "#bfdbfe",
-//   "#ddd6fe",
-//   "#f5d0fe",
-//   "#fed7aa",
-//   "#fee2e2",
-// ];
+import PropTypes from 'prop-types';
 const TAGS = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "Typescript",
-  "Tailwind",
-  "React",
-  "Next.js",
-  "Gatsby",
-  "UI/UX",
-  "SVG",
-  "animation",
-  "webdev",
+	'Innovate',
+	'Enterprenuer',
+	'Build',
+	'Creative',
+	'Distrupt',
+	'Science',
+	'Technology',
+	'Engeneering',
+	'Mathematic',
+	'Empowerment',
+	'Product',
+	'Sustainability',
+	'Inclusion',
+	'collaboration',
+	'Values',
+	'Lunch',
+	'Scale',
+	'UI/UX',
+	'Software',
+	'Mobile app',
+	'Web dev',
+	'Motivation',
+	'Sustainability',
 ];
-const DURATION = 15000;
+const DURATION = 150000;
 const ROWS = 5;
 const TAGS_PER_ROW = 5;
 
@@ -30,61 +32,72 @@ const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 const shuffle = (arr) => [...arr].sort(() => 0.5 - Math.random());
 
 const InfiniteLoopSlider = ({ children, duration, reverse = false }) => {
-  return (
-    <div
-      className="loop-slider"
-      style={{
-        "--duration": `${duration}ms`,
-        "--direction": reverse ? "reverse" : "normal",
-      }}
-    >
-      <div className="inner">
-        {children}
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="loop-slider"
+			style={{
+				'--duration': `${duration}ms`,
+				'--direction': reverse ? 'reverse' : 'normal',
+			}}
+		>
+			<div className="inner">
+				{children}
+				{children}
+			</div>
+		</div>
+	);
 };
 
 InfiniteLoopSlider.propTypes = {
-  children: PropTypes.node.isRequired,
-  duration: PropTypes.number.isRequired,
-  reverse: PropTypes.bool,
+	children: PropTypes.node.isRequired,
+	duration: PropTypes.number.isRequired,
+	reverse: PropTypes.bool,
 };
 
 const Tag = ({ text }) => (
-  <div className="tag">
-    <span>#</span> {text}
-  </div>
+	<div className="tag">
+		<span>#</span> {text}
+	</div>
 );
 
 Tag.propTypes = {
-  text: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
 };
 
 const Animation = () => (
-  <div className="animation">
-    <div className="head">
-      <h1>Infinite Scroll Animation</h1>
-      <p>CSS only, content independent, bi-directional, customizable</p>
-    </div>
-    <div className="tag-list">
-      {[...new Array(ROWS)].map((_, i) => (
-        <InfiniteLoopSlider
-          key={i}
-          duration={random(DURATION - 5000, DURATION + 5000)}
-          reverse={i % 2}
-        >
-          {shuffle(TAGS)
-            .slice(0, TAGS_PER_ROW)
-            .map((tag) => (
-              <Tag text={tag} key={tag} />
-            ))}
-        </InfiniteLoopSlider>
-      ))}
-      <div className="fade" />
-    </div>
-  </div>
+	<div className="animation">
+		<div className="head">
+			<h1 className="text-secondary"> OUR MISSION & VISION</h1>
+			<div className="text-lg font-semibold mt-4 max-w-xl mx-auto leading-relaxed">
+				<p className="">
+					Our mission at StemLab is to help empower women, youth, and
+					marginalized communities through innovative educational programs and
+					entrepreneurial support.
+				</p>
+				<p className="my-6">
+					Our vision is to be a catalyst for technological innovation in
+					Northern Nigeria, creating opportunities and fostering continuous
+					growth through STEM education.
+				</p>
+			</div>
+		</div>
+		<div className="tag-list">
+			{[...new Array(ROWS)].map((_, i) => (
+				<InfiniteLoopSlider
+					key={i}
+					duration={random(DURATION - 5000, DURATION + 5000)}
+					reverse={i % 2}
+				>
+					{shuffle(TAGS)
+						.slice(0, TAGS_PER_ROW)
+						.map((tag) => (
+							<Tag text={tag} key={tag} />
+						))}
+				</InfiniteLoopSlider>
+			))}
+			<div className="fade" />
+		</div>
+	</div>
 );
 
 export default Animation;
